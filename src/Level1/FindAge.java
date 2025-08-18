c8d061254db1f36565cb017cdea5337f461ae07b
Pooja Garg <pujagarg112@gmail.com>
2025-08-19 00:54:13 +0530

    [Pooja] ADD: Program to add 2 numbers.

---
+++ src/Level1/FindAge.java
@@ -0,0 +1,17 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class FindAge {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m= sc.nextInt();
+        int sum= n+m;
+        System.out.println(n);
+        System.out.println(m);
+        System.out.print(sum);
+    }
+}
+
